faff2a7070b5569b61b4f2307587696ad634b99a
yuto141592 <aricchi0208@icloud.com>
2024-06-18 01:26:03 +0900

    second commit

---
+++ myapp/__init__.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+from extensions import db
+from routes import main
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    db.init_app(app)
+    app.register_blueprint(main)
+
+    return app
