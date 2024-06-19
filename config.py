c1f361dd5f5d99f8e126de3901b373310fca53b6
yuto141592 <aricchi0208@icloud.com>
2024-06-19 15:57:15 +0900

    forth commit

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('postgres://imagepicapp_user:ZeXYZxkVbbxX7TcmiyyPLMs6EC9HL2NL@dpg-cpo5coo8fa8c73bbo7hg-a/imagepicapp')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    SECRET_KEY = os.getenv('8e9066bc94cf595ec3fdb445190128e6')
