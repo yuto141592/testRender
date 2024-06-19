a9699ec2f0bd9689311a1c51c1ff2dcd4dba9b50
Yuto Arigasaki <164603623+yuto141592@users.noreply.github.com>
2024-06-19 18:37:16 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ from config import Config
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    #app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     #postgres://imagepicapp_user:ZeXYZxkVbbxX7TcmiyyPLMs6EC9HL2NL@dpg-cpo5coo8fa8c73bbo7hg-a.singapore-postgres.render.com/imagepicapp
-    #app.config.from_object(Config)
+    app.config.from_object(Config)
 
     db.init_app(app)
     app.register_blueprint(main)
